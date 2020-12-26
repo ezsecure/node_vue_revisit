da1c6792caac25d73307e6f72faa8ad4db1990cc
ezsecure <clatar@hanmail.net>
2020-12-26 19:43:35 +0900

    ignore test

--- test/fstest.js
+++ test/fstest.js
@@ -1,14 +1,28 @@
 const fs = require('fs');
+const util = require('util');
 
-fs.readFile('test.json', 'utf-8', (err, data) => {
-        if (err) return console.error(err);
+// console.log("aaa");
+// util.log("bbb", __dirname);
 
-        console.log("data>>", data);
+
+fs.readFile(__dirname + '/test.json', 'utf-8', (err, data) => {
+    if (err) return util.error(err);
+    util.log("data>>", data);
 });
-
 console.log("------------------------");
 
-let data2 = fs.readFileSync('test.json', 'utf-8');
-console.log("data2>>", data2);
+let data2 = fs.readFileSync(__dirname + '/test.json', 'utf-8');
+util.log("data2>>", data2);
 
-console.log("===================================");
+util.log("===================================");
+
+
+let MsgFile = __dirname + '/message.txt';
+fs.writeFileSync(MsgFile, 'Hello Node.js ttt세종대왕', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+
+let data3 = fs.readFileSync(MsgFile, 'utf-8');
+util.log("data3>>", data3);
+util.log("=-=-=-=--=-=-=-=-=-=-=-=-=");
